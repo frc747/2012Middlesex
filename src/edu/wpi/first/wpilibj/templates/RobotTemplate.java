8e46b6ecf5a86a49a77fcacaba52204446aa64cf
mechinn <mechinn@mechinnDesk>
2012-02-23 23:06:03 -0500

    complete manual and simple auto control

--- src/edu/wpi/first/wpilibj/templates/RobotTemplate.java
+++ src/edu/wpi/first/wpilibj/templates/RobotTemplate.java
@@ -1,9 +1,7 @@
-//Chinn's streamlined Simple Robot Template, ill comment stuff on the bus :D
 //Drive motors are Victors, wheel lift motors are Jaguars.(Addison 2/12/12)
 package edu.wpi.first.wpilibj.templates;
 
 import edu.wpi.first.wpilibj.*;
-import edu.wpi.first.wpilibj.buttons.DigitalIOButton;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -15,61 +13,113 @@ import edu.wpi.first.wpilibj.buttons.DigitalIOButton;
 public class RobotTemplate extends RobotBase {
     // Declare variables
     int mode;
-    int drivePercent;
-    double operatorAxisY;
-    
-    //robot drive system
+    int drivePercent, batteryPercent;
+    String autoDrive, autoOperate;
     RobotDrive robotDrive;
-
-    // camera variable
-    //AxisCamera camera;
-    
-    // cRIO info
     DriverStation ds;
-    
-    // timer
     Timer stopwatch;
-    
-    // output to driver console
     DriverStationLCD dsLCD;
-
-    //Encoder
-    Encoder encoder;
-    
-    // Declare variables for the 3 joysticks (right/left for tank, 3rd stick for the operator
     Joystick rightStick, leftStick, operatorStick;
-    
-    // limit switch variable
-    DigitalInput liftLimit1;
-    DigitalInput liftLimit2;
-    DigitalInput liftLimit3;
-    DigitalInput liftLimit4;
-    DigitalInput finLimit1;
-    DigitalInput finLimit2;
-    DigitalInput batteryLimit1;
-    DigitalInput batteryLimit2;
-    
-    //drive motors
-    Victor frontLeftMotor;
-    Victor rearLeftMotor;
-    Victor frontRightMotor;
-    Victor rearRightMotor;
-    
-    //fin motor
+    DigitalInput liftLimitFrontUp, liftLimitFrontDown, liftLimitBackUp, liftLimitBackDown;
+    DigitalInput finLimitFwd, finLimitBck;
+    DigitalInput batteryLimitFwd, batteryLimitBck;
+    Victor frontLeftMotor, rearLeftMotor, frontRightMotor, rearRightMotor;
     Victor finMotor;
-    
-    //battery motor
     Victor batteryMotor;
-    
-    //lift motors
-    Jaguar liftMotor1;
-    Jaguar liftMotor2;
-    
-    //accelerometer
+    Jaguar liftMotorFront;
+    Jaguar liftMotorBack;
     ADXL345_I2C accel;
-    
-    //gyroscope
     Gyro gyro;
+    Encoder batteryEncoder;
+    Encoder finEncoder;
+
+    // constructor for robot variables
+    public RobotTemplate() {
+        super(); // Invokes the RobotBase constructor [aka private RobotBase()]
+        
+        //variables
+        mode = -1;
+        drivePercent = 100;
+        batteryPercent = 100;
+        
+        autoDrive = null;
+        autoOperate = null;
+        
+        // must get instance of these because you are not creating it
+        // it already exists and we need to access it in our program
+        // get the cRIO instance
+        ds = DriverStation.getInstance();
+
+        // get the driver station instance
+        dsLCD = DriverStationLCD.getInstance();
+        
+        // create a stopwatch timer
+        stopwatch = new Timer();
+        
+        /******************************** USB  ********************************/
+        // Define joysticks on the Driver Station
+        rightStick = new Joystick(1);
+        leftStick = new Joystick(2);
+        operatorStick = new Joystick(3);
+        
+        /******************************** PWM  ********************************/
+        //PORTS CAN BE FROM 1-10
+        
+        //setup drive speed controllers
+        frontLeftMotor = new Victor(1);
+        rearLeftMotor = new Victor(2);
+        frontRightMotor = new Victor(4);
+        rearRightMotor = new Victor(5);
+        
+        //setup robot drive class with our drive speed controllers
+        robotDrive = new RobotDrive(frontLeftMotor, rearLeftMotor, frontRightMotor, rearRightMotor);
+        
+        //setup fin speed controller
+        finMotor = new Victor(6);
+        
+        //setup battery speed controller
+        batteryMotor = new Victor(3);
+        
+        //setup lift speed controllers
+        liftMotorFront = new Jaguar(7);
+        liftMotorBack = new Jaguar(8);
+        
+        /**************************** DIGITAL I/O  ****************************/
+        //PORTS CAN BE FROM 1-14
+        
+        //lift limits
+        liftLimitFrontUp = new DigitalInput(1);
+        liftLimitFrontDown = new DigitalInput(2);
+        liftLimitBackUp = new DigitalInput(3);
+        liftLimitBackDown = new DigitalInput(4);
+        
+        //fin limits
+        finLimitFwd = new DigitalInput(5);
+        finLimitBck = new DigitalInput(6);
+        
+        //battery limits
+        batteryLimitFwd = new DigitalInput(7);
+        batteryLimitBck = new DigitalInput(8);
+        
+        //encoder
+        batteryEncoder = new Encoder(9,10);
+        finEncoder = new Encoder(11,12);
+        
+        /***************************** ANALOG I/O *****************************/
+        //PORTS CAN BE FROM 1-8
+        //setup gyro sensor
+        gyro = new Gyro(1);
+        
+        /******************************* OTHER  *******************************/
+        
+        //2Gs of force gives us the most resolution at low forces
+        //which is what we will be dealing with balancing
+        accel = new ADXL345_I2C(1, ADXL345_I2C.DataFormat_Range.k2G);
+        
+        /****************************** COMPLETE ******************************/
+        // let the drivers know that we have contructed the variables
+        dsPrint("Constructor Completed");
+    }
     
     //Driver Station Printing helper functions
     private void dsPrint(String s) {
@@ -82,7 +132,7 @@ public class RobotTemplate extends RobotBase {
 
     private void dsPrint(int line, String s, int column) {
         StringBuffer string = new StringBuffer(s);
-        string.setLength(30);
+        string.setLength(DriverStationLCD.kLineLength); //21 currently
         switch (line) {
             case 6:
                 dsLCD.println(DriverStationLCD.Line.kUser6, column, string);
@@ -111,80 +161,6 @@ public class RobotTemplate extends RobotBase {
             mode = newMode; // set disabled state
         }
     }
-
-    // constructor for robot variables
-    public RobotTemplate() {
-        super(); // Invokes the RobotBase constructor [aka private RobotBase()]
-        
-        mode = -1;
-        drivePercent = 100;
-        
-        // Define joysticks being used at USB port #1 and USB port #2 on the Driver Station
-        rightStick = new Joystick(1);
-        leftStick = new Joystick(2);
-        operatorStick = new Joystick(3);
-        
-        //setup drive speed controllers
-        frontLeftMotor = new Victor(1);
-        rearLeftMotor = new Victor(2);
-        frontRightMotor = new Victor(4);
-        rearRightMotor = new Victor(5);
-        
-        //setup fin speed controller
-        finMotor = new Victor(6);
-        
-        //setup battery speed controller
-        batteryMotor = new Victor(3);
-        operatorAxisY = operatorStick.getAxis(Joystick.AxisType.kY);
-        
-        //setup robot drive class with our drive speed controllers
-        robotDrive = new RobotDrive(frontLeftMotor, rearLeftMotor, frontRightMotor, rearRightMotor);
-        
-        //setup lift speed controllers
-        liftMotor1 = new Jaguar(7);
-        liftMotor2 = new Jaguar(9);
-
-        // must get instance of these because you are not creating it
-        // it already exists and we need to access it in our program
-        // get the cRIO instance
-        ds = DriverStation.getInstance();
-
-        // get the driver station instance
-        dsLCD = DriverStationLCD.getInstance();
-        
-        //encoder
-        encoder = new Encoder(9,10);
-
-        // get the camera instance
-        //camera = AxisCamera.getInstance();
-        
-        // create a stopwatch timer
-        stopwatch = new Timer();
-        
-        //lift limits
-        liftLimit1 = new DigitalInput(1);
-        liftLimit2 = new DigitalInput(2);
-        liftLimit3 = new DigitalInput(3);
-        liftLimit4 = new DigitalInput(4);
-        
-        //fin limits
-        finLimit1 = new DigitalInput(5);
-        finLimit2 = new DigitalInput(6);
-        
-        //battery limits
-        batteryLimit1 = new DigitalInput(7);
-        batteryLimit2 = new DigitalInput(8);
-        
-        //2Gs of force gives us the most resolution at low forces
-        //which is what we will be dealing with balancing
-        accel = new ADXL345_I2C(1, ADXL345_I2C.DataFormat_Range.k2G);
-        
-        //setup gyro sensor
-        gyro = new Gyro(1);
-        
-        // let the drivers know that we have contructed the variables
-        dsPrint("Constructor Completed");
-    }
     
     //set our drive motor bounds uniformly
     private void setDriveBounds(int max, int deadbandMax, int center, int deadbandMin, int min) {
@@ -203,21 +179,64 @@ public class RobotTemplate extends RobotBase {
             setDriveBounds(128+value,128,127,126,127-value);
         }
         //tell the driver the current drive percentage
-        dsPrint(2,drivePercent+"% speed");
+        dsPrint(3, drivePercent+"% speed");
         //take joystick inputs and drive the robot
         robotDrive.tankDrive(leftStick, rightStick);
+        
+        //autolift
+        if(autoDrive != null){
+            if(0 != leftStick.getY() && 0 != rightStick.getY()) {
+                //joysticks in same direction
+                if((leftStick.getY() > 0 && rightStick.getY() > 0) || (leftStick.getY() < 0 && rightStick.getY() < 0)) {
+                    autoDrive = "dwn";
+                } else {
+                    autoDrive = "up";
+                }
+            }
+            setLiftMotors(autoDrive);
+        }
+    }
+    
+    //set our drive motor bounds by percent uniformly and drive the robot
+    private void driveBattery(int percent) {
+        //if our new percent is different than our current one change our bounds
+        if(percent!=batteryPercent) {
+            batteryPercent = percent;
+            int value = 127*batteryPercent/100;
+            batteryMotor.setBounds(128+value,128,127,126,127-value);
+        }
+        //tell the driver the current drive percentage
+        dsPrint(4, batteryPercent+"% speed");
+        //take joystick inputs and drive the robot
+        batteryMotor.set(operatorStick.getY());
     }
     
     private void setLiftMotors(String direction) {
-        if(direction.equals("up")){
-            liftMotor1.set(1);
-            liftMotor2.set(1);
-        } else if (direction.equals("down")){
-             liftMotor1.set(-1);
-             liftMotor2.set(-1);
+        if(direction.equals("up") && (!liftLimitFrontUp.get() || !liftLimitBackUp.get())){
+            liftMotorFront.set(1);
+            liftMotorBack.set(1);
+        } else if (direction.equals("dwn") && (!liftLimitFrontDown.get() || !liftLimitBackDown.get())){
+            liftMotorFront.set(-1);
+            liftMotorBack.set(-1);
         } else {
-            liftMotor1.set(0);
-            liftMotor2.set(0);
+            liftMotorFront.set(0);
+            liftMotorBack.set(0);
+            if(autoDrive!=null) {
+                autoDrive = "stp";
+            }
+        }
+    }
+    
+    private void setFinMotor(String direction) {
+        if(direction.equals("fwd") && (!finLimitFwd.get())){
+            finMotor.set(1);
+        } else if (direction.equals("bck") && (!finLimitBck.get())){
+            finMotor.set(-1);
+        } else {
+            finMotor.set(0);
+            if(autoOperate != null) {
+                autoOperate = "stp";
+            }
         }
     }
     
@@ -235,18 +254,34 @@ public class RobotTemplate extends RobotBase {
         //test sensor data
         {
             ADXL345_I2C.AllAxes axises = accel.getAccelerations();
-            dsPrint(3,axises.XAxis+" "+axises.YAxis+" "+axises.ZAxis);
-            //double gyroValue = gyro.getAngle();
-            double leftValue = leftStick.getY(GenericHID.Hand.kLeft);
-            double rightValue = rightStick.getY(GenericHID.Hand.kRight);
-            boolean limit1 = this.liftLimit1.get();
-            boolean limit2 = this.liftLimit2.get();
-            boolean limit3 = this.liftLimit3.get();
-            boolean limit4 = this.liftLimit4.get();
-            dsPrint(4,Double.toString(leftValue));
-            dsPrint(5,Double.toString(rightValue));
+            dsPrint(5,axises.XAxis+" "+axises.YAxis+" "+axises.ZAxis);
+            
+            boolean limit1 = liftLimitFrontUp.get();
+            boolean limit2 = liftLimitFrontDown.get();
+            boolean limit3 = liftLimitBackUp.get();
+            boolean limit4 = liftLimitBackDown.get();
             dsPrint(6,"1: "+(limit1?"1":"0")+" 2: "+(limit2?"1":"0")+" 3: "+(limit3?"1":"0")+" 4: "+(limit4?"1":"0"));
         }
+        
+        //set auto modes in any mode
+        if(leftStick.getRawButton(3)) {
+            if(autoDrive==null) {
+                autoDrive = "stp";
+            } else {
+                autoDrive = null;
+            }
+        }
+        
+        if (operatorStick.getRawButton(10)){
+            if(autoOperate == null){ 
+                autoOperate = "stp";
+            } else {
+                autoOperate = null;
+            }
+        }
+        
+        //print auto mode
+        dsPrint(2,"Drv: "+autoDrive==null?"off":autoDrive+" Opr: "+autoOperate==null?"off":autoOperate);
         
         // actually print all of the dsPrint strings, send to driver computer
         dsLCD.updateLCD(); 
@@ -254,8 +289,11 @@ public class RobotTemplate extends RobotBase {
 
     //do this while we are disabled
     protected void disabled() {
-        // kill drive
-        robotDrive.tankDrive(0,0);
+        // kill movement
+        robotDrive.drive(0, 0);
+        setLiftMotors("off");
+        setFinMotor("off");
+        batteryMotor.set(0);
     }
 
     //run autonomous code for begining of match
@@ -271,49 +309,43 @@ public class RobotTemplate extends RobotBase {
         } else {
             drive(100);
         }
-        
-        //lift code
-        //TODO: prototype limit switch and lift motor values, most likely will change when testing
-        if (rightStick.getRawButton(3) && (!liftLimit1.get() || !liftLimit3.get())) {
-            setLiftMotors("up");
-        } else if (rightStick.getRawButton(2) && (!liftLimit2.get() || !liftLimit4.get())) {
-            setLiftMotors("down");
+
+        // lift code if we are not automatically doing this lift while driving
+        if(autoDrive == null) {
+            if (rightStick.getRawButton(3)) {
+                setLiftMotors("up");
+            } else if (rightStick.getRawButton(2)) {
+                setLiftMotors("dwn");
+            } else {
+                setLiftMotors("stp");
+            }
+        }
+
+        //fin code
+        if (operatorStick.getRawButton(3) || autoOperate.equals("fwd")){
+            if (autoOperate.equals("fwd")) {
+                autoOperate = "stp";
+            } else if(autoOperate != null) {
+                autoOperate = "fwd";
+            }
+            setFinMotor("fwd");
+        } else if (operatorStick.getRawButton(2) || autoOperate.equals("bck")){
+            if (autoOperate.equals("bck")) {
+                autoOperate = "stp";
+            } else if(autoOperate != null) {
+                autoOperate = "bck";
+            }
+            setFinMotor("bck");
         } else {
-            setLiftMotors("nothing");
+            setFinMotor("stp");
         }
         
-        //fin code
-        /*if (operatorStick.getRawButton(10) == false){*/
-            if (operatorStick.getRawButton(3) && (finLimit1.get())){
-                finMotor.set(1);
-            } else if (operatorStick.getRawButton(2) && (finLimit2.get())){
-                finMotor.set(-1);
-            } else {
-                finMotor.set(0);
-            }
-        /*} else {
-            if (operatorStick.getRawButton(3) && (!finLimit1.get())){
-                finMotor.set(1);
-                if (finLimit1.get()){
-                    finMotor.set(0);
-                }
-            }
-            if (operatorStick.getRawButton(2) && (!finLimit2.get())){
-                finMotor.set(-1);
-                if (finLimit2.get()){
-                    finMotor.set(0);
-                }
-            } 
-         }*/
-        
-         //battery code
-         if (operatorStick.getRawButton(5) && (batteryLimit1.get())){
-             batteryMotor.set(1);
-         } else if (operatorStick.getRawButton(4) && (batteryLimit2.get())){
-             batteryMotor.set(-1);
-         } else {
-             batteryMotor.set(0);
-         }
+        //battery code
+        if(operatorStick.getRawButton(1)) {
+            driveBattery(25);
+        } else {
+            driveBattery(100);
+        }
     }
 
     //main function from our perspective, this is what the robot calls when starting
@@ -325,15 +357,15 @@ public class RobotTemplate extends RobotBase {
             common(); // run common code
             if (isDisabled()) {
                 changeMode(0);
-                dsPrint("Disabled seconds: " + stopwatch.get());
+                dsPrint("Disable " + stopwatch.get() + " sec");
                 disabled(); // run disabled code
             } else if (isAutonomous()) {
                 changeMode(1);
-                dsPrint("Autonomous seconds: " + stopwatch.get());
+                dsPrint("Auto " + stopwatch.get() + " sec");
                 autonomous(); // run the autonomous code
             } else { //if (isTeleoperated()) {
                 changeMode(2);
-                dsPrint("Operator seconds: " + stopwatch.get());
+                dsPrint("Teleop " + stopwatch.get() + " sec");
                 teleoperated(); // run teleoperated code
             }
         } // end while loop SHOULD NEVER HAPPEN OR THE ROBOT IS DEAD
