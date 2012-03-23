e354145adb5a5f3f3e61bb82cd4e8f2f97cdfe19
Michael Chinn <mechinn@gmail.com>
2012-03-23 14:46:19 -0400

    added AutoDrive as an extension of Drive to add encoder support Renamed Team869.java to RobotMain.java to be more clear completed autonomous theoretical code

---
+++ src/com/powercord869/code/robot/AutoDrive.java
@@ -0,0 +1,63 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.powercord869.code.robot;
+
+import edu.wpi.first.wpilibj.Encoder;
+
+/**
+ *
+ * @author mechinn
+ */
+public class AutoDrive extends Drive {
+    private Encoder left, right;
+    private Fin fin;
+    private double desiredCount;
+    private String sig = "auto";
+    
+    public AutoDrive(int fl, int rl, int fr, int rr, int rightA, int rightB, int leftA, int leftB, double diameter, double driveRatio, double distance, int ticks) {
+        super(fl, rl, fr, rr);
+        fin = RobotMain.fin;
+        left = new Encoder(1,leftA,1,leftB,true);
+        right = new Encoder(1,rightA,1,rightB,false);
+        double circumference = Math.PI * diameter;
+        double desiredWheelRev = distance/circumference;
+        double desiredEncoderRev = driveRatio * desiredWheelRev;
+        desiredCount = desiredEncoderRev * ticks;
+    }
+    
+    public void start() {
+        left.start();
+        right.start();
+    }
+    
+    public void reset() {
+        left.reset();
+        right.reset();
+    }
+    
+    public void auto() {
+        int leftCount = left.get();
+        int rightCount = right.get();
+        double avg = (leftCount+rightCount)/2;
+        if(avg < desiredCount) {
+            //simple fin code once we are 75% there
+//            if(avg > desiredCount*.75) {
+//                fin.forward();
+//            }
+            if(leftCount > rightCount) {
+                LCD.print(sig+" right");
+                this.tankDrive(0, .5);
+            } else if(leftCount < rightCount) {
+                LCD.print(sig+" left");
+                this.tankDrive(.5, 0);
+            } else {
+                LCD.print(sig+" forward");
+                this.tankDrive(.5,.5);
+            }
+        } else {
+            LCD.print(sig+" complete");
+        }
+    }
+}
--- src/com/powercord869/code/robot/RobotMain.java
+++ src/com/powercord869/code/robot/RobotMain.java
@@ -13,66 +13,73 @@ import java.io.IOException;
  * creating this project, you must also update the manifest file in the resource
  * directory.
  */
-public class Team869 extends RobotBase {
+public class RobotMain extends RobotBase {
     //Constants    
-    private static final double BATTERY = .5;
-    private static final double FINFWD = .5;
-    private static final double FINBACK = -.4;
-    private static final double LIFT = 1;
+    private final double BATTERY = .5;
+    private final double FINFWD = .5;
+    private final double FINBACK = -.4;
+    private final double LIFT = 1;
     
     //PWMs
         //Drive
-            private static final int frontLeftMotor = 5;
-            private static final int rearLeftMotor = 6;
-            private static final int frontRightMotor = 1;
-            private static final int rearRightMotor = 2;
+            private final int frontLeftMotor = 5;
+            private final int rearLeftMotor = 6;
+            private final int frontRightMotor = 1;
+            private final int rearRightMotor = 2;
         //Lift
-            private static final int liftMotorFront = 8;
-            private static final int liftMotorBack = 7;
+            private final int liftMotorFront = 8;
+            private final int liftMotorBack = 7;
         //Fin
-            private static final int finMotor = 3;
+            private final int finMotor = 3;
         //Weight
-            private static final int batteryMotor = 4;
+            private final int batteryMotor = 4;
     //DI/Os
         //Lift
-            private static final int liftLimitFrontUp = 3;
-            private static final int liftLimitFrontDown = 4;
-            private static final int liftLimitBackUp = 2;
-            private static final int liftLimitBackDown = 1;
+            private final int liftLimitFrontUp = 3;
+            private final int liftLimitFrontDown = 4;
+            private final int liftLimitBackUp = 2;
+            private final int liftLimitBackDown = 1;
         //Fin
-            private static final int finLimitForward = 8;
-            private static final int finLimitBack = 9;
+            private final int finLimitForward = 8;
+            private final int finLimitBack = 9;
         //Weight
-            private static final int batteryLimitFwd = 10;
-            private static final int batteryLimitBck = 7;
+            private final int batteryLimitFwd = 10;
+            private final int batteryLimitBck = 7;
+        //Encoder
+            private final int rightA = 11;
+            private final int rightB = 12;
+            private final int leftA = 13;
+            private final int leftB = 14;
+            
+    //Other
+        //Encoder Values
+            private final double diameter = 6;
+            private final double driveRatio = 26.0/12.0;
+            private final double distance = 10;
+            private final int ticks = 250;
             
     // Declare variables SO MANY VARIABLES!!!
     private int mode;
-    private DriverStation ds;
-    private AxisCamera camera;
-    private Timer stopwatch;
-    private Joystick leftStick, rightStick, operatorStick;
-    private FRCKinect kinect;
+    public static DriverStation ds;
+    public static AxisCamera camera;
+    public static Timer stopwatch;
+    public static Joystick leftStick, rightStick, operatorStick;
+    public static FRCKinect kinect;
     
     private boolean recording;
     private Recorder recorder;
     private Loader loader;
     
-    private Drive drive;
-    private Lift lift;
-    private Fin fin;
-    private WeightShifter weight;
-    
-    //unused right now
-    private ADXL345_I2C accel;
-    private Gyro gyro;
-    private Encoder left, right;
+    public static AutoDrive drive;
+    public static Lift lift;
+    public static Fin fin;
+    public static WeightShifter weight;
     
     //autonomous flags
     private boolean prepLeft, prepRight;
 
     // constructor for robot variables
-    public Team869() {
+    public RobotMain() {
         super(); // Invokes the RobotBase constructor [aka private RobotBase()]
         
         //variables
@@ -95,7 +102,7 @@ public class Team869 extends RobotBase {
         prepLeft = false;
         prepRight = false;
         
-        drive = new Drive(frontLeftMotor, rearLeftMotor, frontRightMotor, rearRightMotor);
+        drive = new AutoDrive(frontLeftMotor, rearLeftMotor, frontRightMotor, rearRightMotor, rightA, rightB, leftA, leftB, diameter, driveRatio, distance, ticks);
         lift = new Lift(LIFT, liftMotorFront, liftMotorBack, liftLimitFrontUp, liftLimitFrontDown, liftLimitBackUp, liftLimitBackDown);
         fin = new Fin(FINFWD, FINBACK, finMotor, finLimitForward, finLimitBack);
         weight = new WeightShifter(batteryMotor, batteryLimitFwd, batteryLimitBck);
@@ -113,23 +120,13 @@ public class Team869 extends RobotBase {
         /**************************** DIGITAL I/O  ****************************/
         //PORTS CAN BE FROM 1-14
         
-        //encoder
-        right = new Encoder(1,11,1,12,true);
-        left = new Encoder(1,13,1,14,false);
-        
-        //1pulse / (425 pulses per revolution / 2*PI*r circumference )
-        right.setDistancePerPulse((double)1/(425/(2*Math.PI*3)));
         
         /***************************** ANALOG I/O *****************************/
         //PORTS CAN BE FROM 1-8
-        //setup gyro sensor
-        gyro = new Gyro(1,1);
+        
         
         /******************************* OTHER  *******************************/
         
-        //2Gs of force gives us the most resolution at low forces
-        //which ifs what we will be dealing with balancing
-        accel = new ADXL345_I2C(1, ADXL345_I2C.DataFormat_Range.k2G);
         
         /****************************** COMPLETE ******************************/
         // let the drivers know that we have contructed the variables
@@ -177,8 +174,7 @@ public class Team869 extends RobotBase {
     protected void robotInit() {
         stopwatch.start();
         
-        right.start();
-        left.start();
+        drive.start();
         
         // let the drivers know that we have initialized the robot
         LCD.print("Robot Initialized");
@@ -187,28 +183,26 @@ public class Team869 extends RobotBase {
     //common code to be run in all modes every cycle
     protected void common(){
         //makes sure we are not on the field
-        if(!ds.isFMSAttached()) {
-            try {
-            //if we press the button once start recording, press it again close the recording
-                if(operatorStick.getRawButton(11)) {
-                    if(!recording) {
-                        recorder = new Recorder("file:///auto1.log");
-                        recording = true;
-                    } 
-                } else if(operatorStick.getRawButton(10)) {
-                    if(recording) {
-                        recorder.close();
-                        recording = false;
-                    } 
-                }
-            } catch (IOException ex) {
-                ex.printStackTrace();
-            }
-        } else {
-            recording = false;
-        }
-        
-        System.out.println("right: "+right.getDistance());
+//        if(!ds.isFMSAttached()) {
+//            try {
+//            //if we press the button once start recording, press it again close the recording
+//                if(operatorStick.getRawButton(11)) {
+//                    if(!recording) {
+//                        recorder = new Recorder("file:///auto1.log");
+//                        recording = true;
+//                    } 
+//                } else if(operatorStick.getRawButton(10)) {
+//                    if(recording) {
+//                        recorder.close();
+//                        recording = false;
+//                    } 
+//                }
+//            } catch (IOException ex) {
+//                ex.printStackTrace();
+//            }
+//        } else {
+//            recording = false;
+//        }
         
         LCD.update(); 
     }
@@ -226,8 +220,7 @@ public class Team869 extends RobotBase {
         //commenting out for now, very possibly could break things
 //        calebrateGyro();
         
-        right.reset();
-        left.reset();
+        drive.reset();
     }
 
     //run autonomous code for begining of match
@@ -254,8 +247,8 @@ public class Team869 extends RobotBase {
             }
         } else {
             //if the person is not in front or not with arms out to their side kill drive
-            drive.stop();
             fin.stop();
+            drive.auto();
         }
         //kill everything else
         lift.stop();
